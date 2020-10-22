3bf546908cf1f28a202c0355157b42e606af510c
Kihyun Kenny Hong <kh3073@columbia.edu>
2020-10-22 13:14:49 +0000

    Sightings urls and views added/modified

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Sighting
+
+def all_sightings(request):
+    squirrels = Sighting.objects.all()
+    context = {
+            'squirrels': squirrels,
+            }
+    return render(request, 'sightings/all_sightings.html', context)
+
