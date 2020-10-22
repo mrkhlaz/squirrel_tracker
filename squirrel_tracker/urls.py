b0b3d2cab84f94b3fb21ef6a6abd2fe04e2a7f8b
Kihyun Kenny Hong <kh3073@columbia.edu>
2020-10-22 03:36:31 +0000

    Changed settings and urls

--- squirrel_tracker/urls.py
+++ squirrel_tracker/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('squirrel.urls')),
-]
+        path('admin/', admin.site.urls),
+        path('map/',include('map.urls')),
+        path('sightings/',include('sightings.urls')),
+        # path('', include('squirrel.urls')),
+        ]
