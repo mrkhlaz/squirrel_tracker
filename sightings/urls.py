5e70d6505201962fa442a50fb465865c2b872429
Kihyun Kenny Hong <kh3073@columbia.edu>
2020-10-23 03:56:44 +0000

    Added forms and some views

--- sightings/urls.py
+++ sightings/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'sightings'
 urlpatterns = [
     path('', views.all_sightings),
-    #path('add/', views.add, name='add'),
-    #re_path(r'(?P<squirrel_id>[0-9]+[a-zA-Z]-[a-zA-Z]M-[0-9]{4}-[0-9]{2})/$', views.article_detail),
-    #path('stats/', views.stats, name='stats'),
+    path('add/', views.add_sighting),
+    re_path(r'(?P<squirrel_id>[0-9]+[a-zA-Z]-[a-zA-Z]M-[0-9]{4}-[0-9]{2})/$', views.update_sighting),
+    #path('stats/', views.stats),
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -1,19 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
 from .models import Sighting
+from .forms import SightingForm
 
 def all_sightings(request):
     squirrels = Sighting.objects.all()
     context = {
-        'squirrels': squirrels,
-    }
+            'squirrels': squirrels,
+            }
     return render(request, 'sightings/all_sightings.html', context)
 
-#def detail(request, squirrel_id):
-    #sighting = get_object_or_404(___, pk=squirrel_id) #Add index for data
-    #context = {
-        #'sighting': sighting,
-    #}
-    #return render(request, 'sightings/detail.html', context)
+def add_sighting(request):
+    if request.method == 'POST':
+	form = SightingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/sightings")
+    else:
+	form = SightingForm()
+
+    context = {
+            'form':form
+            }
+    return render(request, 'sightings/add_sighting.html', context)
+
+def update_sighting(request, squirrel_id):
+    sighting = get_object_or_404(Sighting, unique_squirrel_id = squirrel_id)
+    if request.method == 'POST':
+        form = SightingForm(request.POST, instance=sighting)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/{unique_squirrel_id}')
+    else:
+        form = SightingForm(instance=sighting)
+    
+    context = {
+            'form':form
+            }
+
+    return render(request, 'sightings/update_sighting.html', context)
