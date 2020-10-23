dc96d2e08e0374568b950c6824d9b95bf84c9d12
etzn <72319597+etzn@users.noreply.github.com>
2020-10-22 21:00:31 -0400

    Define detail subpages
    Prepping for adding in content I've been working on from prior repo. New material was tested separately but is commented out for now until everything works.

--- sightings/urls.py
+++ sightings/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'sightings'
 urlpatterns = [
-         path('', views.all_sightings),
-         ]
+    path('', views.all_sightings),
+    #path('add/', views.add, name='add'),
+    #path('<int:squirrel_id>/', views.detail, name='detail'),
+    #path('stats/', views.stats, name='stats'),
+]
--- sightings/views.py
+++ sightings/views.py
@@ -7,7 +7,13 @@ from .models import Sighting
 def all_sightings(request):
     squirrels = Sighting.objects.all()
     context = {
-            'squirrels': squirrels,
-            }
+        'squirrels': squirrels,
+    }
     return render(request, 'sightings/all_sightings.html', context)
 
+#def detail(request, squirrel_id):
+    #sighting = get_object_or_404(___, pk=squirrel_id)
+    #context = {
+        #'sighting': sighting,
+    #}
+    #return render(request, 'sightings/detail.html', context)
