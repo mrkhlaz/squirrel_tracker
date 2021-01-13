422023febc9d552db5bf29002e1471c729f43b1c
Kihyun "Kenny" Hong <65016737+mrkhlaz@users.noreply.github.com>
2021-01-13 02:28:22 -0500

    Update settings.py

--- squirrel_tracker/settings.py
+++ squirrel_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y7k&34azo8ronxft2t-nm1)-5e*otr2vy4!!^s4#11qj*q%+5n'
+SECRET_KEY = 'HIDDEN'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
